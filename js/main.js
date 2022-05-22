34b917902bb38e13aca6502d49ab7fcdb2e5bd4f
Abdulaziz-Abdussomat05 <abdussomatabdulaziz7@gmail.com>
2022-05-21 21:11:45 -1200

    Second website

--- js/main.js
+++ js/main.js
@@ -1,2 +1,44 @@
 "use strict"
+window.addEventListener("DOMContentLoaded", () => {
+    const loader = document.querySelector(".loader")
+    setTimeout(function() {
+        loader.style.opacity = "0"
+         setTimeout(function(){
+            loader.style.display = "none"
+        },1500)
+    },2000)
 
+    const alModalBtn = document.querySelectorAll("[data-modal]"),
+modal = document.querySelector(".modal"),
+modalClose = document.querySelector("[data-close]")
+
+alModalBtn.forEach((btn) => {
+  btn.addEventListener("click",openModal )
+})
+
+function openModal() {
+  modal.classList.add("show")
+  modal.classList.remove("hide")
+  document.body.style.overflow = "hidden"
+  clearInterval(timeModalOut)
+}
+
+function closeModal() {
+  modal.classList.add("hide")
+  modal.classList.remove("show")
+  document.body.style.overflow = "";
+ 
+
+}
+
+modalClose.addEventListener("click", closeModal)
+
+modal.addEventListener("click", (e) => {
+  if(e.target === modal) {
+    closeModal()
+  }
+})
+
+const timeModalOut = setTimeout(openModal, 5000)
+
+})
